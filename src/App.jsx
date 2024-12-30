d1bc618705ad9b40147fa5cf3b7ac6b4c3d220ad
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2024-12-30 11:15:10 +0600

    Project Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
+    </>
+  );
+}
+
+export default App;
